cafa9a7efce4ae4199bf65c546bb0d532367e0d0
Fab <venier.fabien@outlook.fr>
2021-02-02 14:42:29 +0100

    add img for training

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
-import cv2
-# define a video capture object
-vid = cv2.VideoCapture(0)
+from cv2 import cv2
+import sys
 
-while(True):
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
+cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame', frame)
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    cv2.imshow('Video', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
