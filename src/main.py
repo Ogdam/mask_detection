cd0ff61c4f037eca8f26e5d003fc81444bf80f7f
Fab <venier.fabien@outlook.fr>
2021-02-03 11:48:07 +0100

    create branch

--- src/main.py
+++ src/main.py
@@ -14,8 +14,9 @@ video_capture = cv2.VideoCapture(0)
 
 model = models.load_model('./weight/mask.model')
 model.load_weights('./weight/mask.h5')
+probability_model = tf.keras.Sequential([model])
 
-class_names = ["mask", "no mask"]
+class_names = [" mask", " no mask", " bad position"]
 
 while True:
     # Capture frame-by-frame
@@ -24,32 +25,33 @@ while True:
     faces = faceCascade.detectMultiScale(gray,
                                          scaleFactor=1.1,
                                          minNeighbors=5,
-                                         minSize=(60, 60),
+                                         minSize=(64, 64),
                                          flags=cv2.CASCADE_SCALE_IMAGE)
-    for (x,y,w,h) in faces:
-        color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        roi = color[x:w, y:h]
-
+    for face in faces:
+        (x,y,w,h) = face
+        roi = frame[x:w, y:h]
         if len(roi) > 0 :
             try:
                 output = []
-                data = fff.resize(roi, (32,32))
+                data = fff.resize(roi, (64,64))
                 output.append(data)
                 npRoi = np.array(output)
                 npRoi = npRoi / 255.0
-                probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
                 predictions = probability_model.predict(npRoi)
                 predict  = np.argmax(predictions)
-
                 if predict == 1:
                     cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
-                else :
+                    cv2.rectangle(frame, (x-1, y-25), (x+100, y),(0,255,0), -1)
+                elif predict == 2 :
                     cv2.rectangle(frame, (x, y), (x + w, y + h),(255,0,0), 2)
-                print(class_names[predict])
-
-                cv2.imshow('Video', frame)
-            except:
+                    cv2.rectangle(frame, (x-1, y-25), (x+100, y),(255,0,0), -1)
+                else :
+                    cv2.rectangle(frame, (x, y), (x + w, y + h),(0,0,255), 2)
+                    cv2.rectangle(frame, (x-1, y-25), (x+100, y),(0,0,255), -1)
+                cv2.putText(frame, class_names[predict], (x, y-10), cv2.FONT_HERSHEY_PLAIN, 0.9, (0,0,0), 2)
+            except Exception as e:
                 pass
+    cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- src/train_model.py
+++ src/train_model.py
@@ -15,34 +15,32 @@ train_labels = []
 test_images = []
 test_labels = []
 
-FOLDER_TRAIN = './dataset/train/'
+FOLDER_TRAIN = './dataset/train'
 FOLDER_TEST = './dataset/test/'
 
-for file in os.listdir(FOLDER_TRAIN+"with_mask/"):
-    src = cv2.imread(FOLDER_TRAIN+"with_mask/"+file)
-    output = cv2.resize(src, (32,32))
+for file in os.listdir(FOLDER_TRAIN):
+    src = cv2.imread(FOLDER_TRAIN+"/"+file)
+    output = cv2.resize(src, (64,64))
     train_images.append(output)
-    train_labels.append(0)
+    if "no_mask" in file:
+        train_labels.append(1)
+    elif "bad_mask" in file:
+        train_labels.append(2)
+    else:
+        train_labels.append(0)
 
-for file in os.listdir(FOLDER_TRAIN+"without_mask/"):
-    src = cv2.imread(FOLDER_TRAIN+"without_mask/"+file)
-    output = cv2.resize(src, (32,32))
-    train_images.append(output)
-    train_labels.append(1)
-
-for file in os.listdir(FOLDER_TEST+"mask/"):
-    src = cv2.imread(FOLDER_TEST+"mask/"+file)
-    output = cv2.resize(src, (32,32))
+for file in os.listdir(FOLDER_TEST):
+    src = cv2.imread(FOLDER_TEST+"/"+file)
+    output = cv2.resize(src, (64,64))
     test_images.append(output)
-    test_labels.append(0)
+    if "no_mask" in file:
+        test_labels.append(1)
+    elif "bad_mask" in file:
+        test_labels.append(2)
+    else:
+        test_labels.append(0)
 
-for file in os.listdir(FOLDER_TEST+"no_mask/"):
-    src = cv2.imread(FOLDER_TEST+"no_mask/"+file)
-    output = cv2.resize(src, (32,32))
-    test_images.append(output)
-    test_labels.append(1)
-
-class_names = ["mask", "no mask"]
+class_names = ["mask", "no mask", "bad position"]
 
 train_labels = np.array(train_labels)
 train_images = np.array(train_images)
@@ -72,8 +70,10 @@ plt.show()
 model = models.Sequential()
 model.add(layers.Flatten(input_shape=train_images.shape[1:]))
 model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(2, activation='sigmoid'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(3))
 
 #Compilez le modèle
 model.compile(optimizer='adam',
@@ -84,7 +84,7 @@ log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 #Former le modèle
-model.fit(train_images, train_labels, epochs=10,
+model.fit(train_images, train_labels, epochs=7,
           validation_data=(test_images, test_labels),
           callbacks=[tensorboard_callback])
 
@@ -119,10 +119,10 @@ def plot_image(i, predictions_array, true_label, img):
 def plot_value_array(i, predictions_array, true_label):
   true_label = true_label[i]
   plt.grid(False)
-  plt.xticks(range(2))
+  plt.xticks(range(3))
   plt.yticks([])
-  thisplot = plt.bar(range(2), predictions_array, color="#777777")
-  plt.ylim([0, 1])
+  thisplot = plt.bar(range(3), predictions_array, color="#777777")
+  plt.ylim([0, 2])
   predicted_label = np.argmax(predictions_array)
 
   thisplot[predicted_label].set_color('red')
